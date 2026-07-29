bc7f02741d2575510057418ffa5315ef127cd203
Akos Orban <akos.orban@gmail.com>
2026-07-29 16:46:59 +0200

    mna 0.1.0: rebuilt binaries (version fix)

--- Formula/mna.rb
+++ Formula/mna.rb
@@ -7,16 +7,16 @@ class Mna < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/MantaCodeDevs/mna-cli/releases/download/v0.1.0/mna-darwin-arm64.tar.gz"
-      sha256 "3e0e281e7c54e58ddb544fac813168bbc1ca6d7efb49c5bc92d8b3585599fe24"
+      sha256 "924c422abb56a547f718b2f36aeb42f7e0ea71a24322a49bb7426cdddefa036e"
     else
       url "https://github.com/MantaCodeDevs/mna-cli/releases/download/v0.1.0/mna-darwin-x64.tar.gz"
-      sha256 "a51ccc4bdbcebea613b62744ecb77a662aaed45272ccc13190820199a6948546"
+      sha256 "a53eada01d23e7eecf557bfbacde9e5db9d96236014ad60c91779b68421112f9"
     end
   end
 
   on_linux do
     url "https://github.com/MantaCodeDevs/mna-cli/releases/download/v0.1.0/mna-linux-x64.tar.gz"
-    sha256 "60afd31d9b541894ca0f4ea93495926bec341f03580c5138cbc993c7cbf04b0c"
+    sha256 "199a476fbe15b1c74610ee03dba8daa1ce1515b5fa8fbe8bb090d690b69e9683"
   end
 
   def install
